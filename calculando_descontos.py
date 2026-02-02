fc637db4ad03e61d9b1518661efcb684d81ddb1d
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-02 15:24:07 -0300

    Lição_11_descontos

---
+++ calculando_descontos.py
@@ -0,0 +1,5 @@
+prod = float(input('Qual o preço do produto? R$'))
+avista = prod - (prod*0.05)
+parc = prod + (prod*0.08)
+print('O produto que custa R${:.2f}, a vista ira ter um desconto de 5%, custando agora R${:.2f}'.format(prod,avista))
+print('Caso queira parcelar, o produto que custava R${:.2f}, ira ter um amento de 8%, no total será R${:.2f}'.format(prod,parc))
