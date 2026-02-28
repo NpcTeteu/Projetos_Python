cb99fcf4709774dc014555e20c06142133645118
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-28 15:16:43 -0300

    Mudança de nome e adição de comentário

--- ex_016.py
+++ ex_016.py
@@ -1,5 +1,7 @@
 import math
 cate_ad = float(input("Digite o cateto adjacente: "))
 cate_op = float(input("Digite o cateto oposto: "))
+# O Teorema de Pitágoras está também na bibliotaca math
 hip = math.hypot(cate_ad,cate_op)
+
 print(hip)
