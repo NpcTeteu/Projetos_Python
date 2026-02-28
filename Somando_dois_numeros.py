417557a898f9620c98cdef5a705975786330c8ce
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-28 14:40:57 -0300

    Mudança de nome e adição de comentário

--- Somando_dois_numeros.py
+++
@@ -1,3 +0,0 @@
-user_num1 = int(input("Digite um numero: "))
-user_num2 = int(input("Digite outro numero: "))
-print("A soma desses dois numeros é {}".format(user_num1+user_num2))
---
+++ ex_003.py
@@ -0,0 +1,5 @@
+user_num1 = int(input("Digite um numero: "))
+user_num2 = int(input("Digite outro numero: "))
+
+# As chaves servem para dizer onde a variavel deve aparecer e o format coloca oque está dentro do parenteses nessas chaves
+print("A soma desses dois numeros é {}".format(user_num1+user_num2))
