36bf1d6bb1719af825098f0ee1f6e618a43a6eae
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-28 14:59:32 -0300

    Mudança de nome e adição de comentário

--- ex_010.py
+++ ex_010.py
@@ -1,6 +1,9 @@
 larg = float(input("Qual a largura de sua parede em metros: "))
 comp = float(input("Qual o comprimento de sua parede em metros: "))
+# Calculo para a Área
 dimen = float (larg*comp)
+# A cada 2 mestros equivale a 1 Litro de Tinta
 tinta = dimen / 2
 print('Sua parede tem a dimensão de {:.2f}m x {:.2f}m e a sua área é de {:.2f}m²'.format(larg,comp,dimen))
+
 print('Para pintar sua parde é necessário {:.2f}L de tinta'.format(tinta))
